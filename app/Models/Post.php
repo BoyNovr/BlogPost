82aeb68eebdf85e2db7bca7be05a7e58592fbe04
BoyNovr <boynovriyal2@gmail.com>
2024-03-24 15:47:27 +0700

    Searching & Pagination

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,33 @@ class Post extends Model
         'title','slug','category_id','excerpt','body'
     ];
     protected $with=['category','author'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        // if(isset($filters['search'])? $filters['search']: false)
+        // {
+        //     return $query->where('title', 'like', '%' . $filters['search'] . '%')
+        //     ->orWhere('body','like', '%'. $filters['search'] . '%');
+        // }
+        //cara lain
+        $query->when($filters['search'] ?? false, function($query, $search){
+        return $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('body','like', '%'. $search . '%');
+        });
+
+        $query->when($filters['category']?? false, function($query, $category){
+            return $query->whereHas('category',function($query) use($category){
+                $query->where('slug',$category);
+            });
+        });
+
+        $query->when($filters['author']?? false, fn($query,$author)=>
+        $query->whereHas('author', fn($query)=>
+            $query->where('username',$author)
+            )
+            );
+    }
+    
     // protected $guarded=['id']; gk boleh di isi sisanya boleh
     public function category()
     {
@@ -23,3 +50,4 @@ class Post extends Model
         return $this->belongsTo(User::class,'user_id');
     }
 }
+
